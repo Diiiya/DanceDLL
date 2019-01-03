656fbd51aa38314487403e28be162b718c68b334
Diiiya <33103806+Diiiya@users.noreply.github.com>
2019-01-03 14:50:31 +0200

    A simple C# model class

---
+++ DanceDLL/Dance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DanceDLL
+{
+    public class Dance
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Photo { get; set; }
+        public string Country { get; set; }
+        public int TimeAppeared { get; set; }
+        public string Type { get; set; }
+        public DateTime AddedDate { get; set; }
+    }
+}

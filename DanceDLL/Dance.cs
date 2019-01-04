a151f26ad49dca7dffb60ee6bc723f5dad8d2b8a
Diiiya <33103806+Diiiya@users.noreply.github.com>
2019-01-04 15:12:57 +0200

    ...

--- DanceDLL/Dance.cs
+++ DanceDLL/Dance.cs
@@ -5,12 +5,12 @@ namespace DanceDLL
     public class Dance
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string DName { get; set; }
+        public string DDescription { get; set; }
         public string Photo { get; set; }
         public string Country { get; set; }
         public int TimeAppeared { get; set; }
-        public string Type { get; set; }
+        public string DType { get; set; }
         public DateTime AddedDate { get; set; }
     }
 }
